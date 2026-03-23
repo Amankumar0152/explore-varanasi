cbdd17c793302b7019e7af7f1165430ab65f3a6d
Aman Kumar <amankumarmaurya93@gmail.com>
2026-03-23 23:38:49 +0530

    Add Navbar in tours

--- js/include.js
+++ js/include.js
@@ -1,10 +1,30 @@
 // js/include.js
-document.addEventListener('DOMContentLoaded', function () {
+// document.addEventListener('DOMContentLoaded', function () {
   // Load the footer HTML
-  fetch('components/footer.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('footer').innerHTML = data;
-    })
-    .catch(error => console.error('Error loading footer:', error));
+//   fetch('components/footer.html')
+//     .then(response => response.text())
+//     .then(data => {
+//       document.getElementById('footer').innerHTML = data;
+//     })
+//     .catch(error => console.error('Error loading footer:', error));
+// });
+
+
+// js/include.js
+document.addEventListener('DOMContentLoaded', function () {
+    // Load the navbar
+    fetch('/components/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading navbar:', error));
+
+    // Load the footer
+    fetch('/components/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading footer:', error));
 });
